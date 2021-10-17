d4cea271e883f23208e02a03fafd5fa21bdc47cf
angel77-lina <83407551+angel77-lina@users.noreply.github.com>
2021-10-17 16:03:07 +0700

    Update detailActivity.kt

--- pertemuan7_71190506/app/src/main/java/id/ac/ukdw/pertemuan7_71190506/detailActivity.kt
+++ pertemuan7_71190506/app/src/main/java/id/ac/ukdw/pertemuan7_71190506/detailActivity.kt
@@ -1,4 +1,29 @@
 package id.ac.ukdw.pertemuan7_71190506
 
-class detailActivity {
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class detailActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        val nama = intent.getStringExtra("nama")
+        val noHP =intent.getStringExtra("noHP")
+        val tertaut = intent.getIntExtra("noHP",0)
+        val foto = intent.getIntExtra("foto",0)
+
+        val showNama = findViewById<TextView>(R.id.txtShowNama)
+        val showNoHp = findViewById<TextView>(R.id.txtShowNoHp)
+        val showTertaut = findViewById<TextView>(R.id.txtShowTertaut)
+        val showFoto = findViewById<ImageView>(R.id.imgShowKontak)
+
+        showNama.text = nama
+        showNoHp.text = noHP
+        showTertaut.setText(tertaut)
+        showFoto.setImageResource(foto)
+    }
+
 }
